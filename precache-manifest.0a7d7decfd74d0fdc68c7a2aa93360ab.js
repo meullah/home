b14b0b25ead33dec00e2088843d32300d9924899
meullah <mehsanullah24@gmail.com>
2020-09-03 23:26:42 +0500

    Updates

---
+++ precache-manifest.0a7d7decfd74d0fdc68c7a2aa93360ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47b11c32d3a3f002c676a5da2a29ca9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "439b03308cf152b674a1",
+    "url": "/home/static/css/main.669ac10a.chunk.css"
+  },
+  {
+    "revision": "7729da24cb14344f8f6e",
+    "url": "/home/static/js/2.726bd328.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/static/js/2.726bd328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439b03308cf152b674a1",
+    "url": "/home/static/js/main.a99f87f2.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "fb87c558b88a95a962173a773f68f287",
+    "url": "/home/static/media/pp.fb87c558.jpg"
+  },
+  {
+    "revision": "7be4bfeebbfe248170661541d3446e74",
+    "url": "/home/static/media/resume.7be4bfee.pdf"
+  }
+]);
